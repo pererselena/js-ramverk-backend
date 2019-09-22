d90d38594f98a2c2320b7fd784d71c8c4ec9b227
Elena Perers <elena.perers@chasacademy.se>
2019-09-22 16:48:38 +0200

    change me text

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,15 @@ router.get('/', function(req, res, next) {
             title: "Min me-sida i kursen jsramverk",
             msg: `![Bild på mig class=me](https://elenaperers.me/img/profileImg.jpg) { width=500}
 
-                        Mitt namn är Elena Perers. Född och uppvuxen i Barnaul, en stad som ligger Sydvästra Sibirien i Ryssland. Där växte jag upp, gick i skolan och universitetet.
+Mitt namn är Elena Perers. Född och uppvuxen i Barnaul, en stad som ligger Sydvästra Sibirien i Ryssland. Där växte jag upp, gick i skolan och universitetet.
 
-                        Jag läste sociologi och juridik på Altai State University i Barnaul, Ryssland.
+Jag läste sociologi och juridik på Altai State University i Barnaul, Ryssland.
 
-                        Mitt första möte med programmering fick jag i högstadiet då vi fick lära oss basic. Det kändes för komplicerat för mig på den tiden och bestämde mig för att inte bli utvecklare. Men nu sitter jag här och studerar webbprogramering. Intresset väcktes för inte så länge sedan när jag flyttade till Sverige bestämde jag mig för att byta yrke helt. Jag började bli nyfiken på hur fungerar webbsidor och började titta på video kurser om HTML, CSS och JS och blev förvånad att det är inte så komplicerat och det är inte omöjligt att lära sig.
+Mitt första möte med programmering fick jag i högstadiet då vi fick lära oss basic. Det kändes för komplicerat för mig på den tiden och bestämde mig för att inte bli utvecklare. Men nu sitter jag här och studerar webbprogramering. Intresset väcktes för inte så länge sedan när jag flyttade till Sverige bestämde jag mig för att byta yrke helt. Jag började bli nyfiken på hur fungerar webbsidor och började titta på video kurser om HTML, CSS och JS och blev förvånad att det är inte så komplicerat och det är inte omöjligt att lära sig.
 
-                        Vill du läsa mer om vad jag jobbat med så kan du kika på min [Linkedin-profil](https://www.linkedin.com/in/elena-perers/)Linkedin-profil.
+Vill du läsa mer om vad jag jobbat med så kan du kika på min [Linkedin-profil](https://www.linkedin.com/in/elena-perers/)Linkedin-profil.
 
-                        /Elena Perers`
+/Elena Perers`
         }
     };
 
