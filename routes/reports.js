9f04945782b4851ca9c8ab26cb552be888096d91
Elena Perers <elena.perers@chasacademy.se>
2019-09-22 15:03:38 +0200

    Added routes for auth.

--- routes/reports.js
+++ routes/reports.js
@@ -3,12 +3,19 @@
 var express = require('express');
 var router = express.Router();
 var reports = require("../models/reports.js");
+const auth = require("../models/auth.js");
 
 
 router.get('/', (req, res) => reports.getAllReports(res));
-router.post('/', (req, res) => reports.createReport(res, req.body));
-router.put('/', (req, res) => reports.updateReport(res, req.body));
-router.delete('/', (req, res) => reports.deleteReport(res, req.body));
+router.post('/',
+    (req, res, next) => auth.checkToken(req, res, next),
+    (req, res) => reports.createReport(res, req.body));
+router.put('/',
+    (req, res, next) => auth.checkToken(req, res, next),
+    (req, res) => reports.updateReport(res, req.body));
+router.delete('/',
+    (req, res, next) => auth.checkToken(req, res, next),
+    (req, res) => reports.deleteReport(res, req.body));
 
 
 module.exports = router;
